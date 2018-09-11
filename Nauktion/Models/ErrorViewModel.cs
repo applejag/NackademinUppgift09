23abf034bad959570527cc86d025f29ca1375b2d
kalle (jag) <kalle.jillheden@me.com>
2018-09-11 10:12:27 +0200

    Project setup

---
+++ Nauktion/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Nauktion.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
