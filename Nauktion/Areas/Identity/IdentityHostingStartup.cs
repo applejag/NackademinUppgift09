f760534626514a49b9183d32f9532e49be947335
kalle (jag) <kalle.jillheden@me.com>
2018-09-11 20:19:13 +0200

    Added Identity scaffhold

---
+++ Nauktion/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Nauktion.Areas.Identity.Data;
+using Nauktion.Models;
+
+[assembly: HostingStartup(typeof(Nauktion.Areas.Identity.IdentityHostingStartup))]
+namespace Nauktion.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<NauktionContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("NauktionContextConnection")));
+
+                services.AddDefaultIdentity<NauktionUser>()
+                    .AddEntityFrameworkStores<NauktionContext>();
+            });
+        }
+    }
+}
---
+++ Nauktion/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Nauktion.Areas.Identity.Data;
+
+namespace Nauktion.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class LogoutModel : PageModel
+    {
+        private readonly SignInManager<NauktionUser> _signInManager;
+        private readonly ILogger<LogoutModel> _logger;
+
+        public LogoutModel(SignInManager<NauktionUser> signInManager, ILogger<LogoutModel> logger)
+        {
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPost(string returnUrl = null)
+        {
+            await _signInManager.SignOutAsync();
+            _logger.LogInformation("User logged out.");
+            if (returnUrl != null)
+            {
+                return LocalRedirect(returnUrl);
+            }
+            else
+            {
+                return Page();
+            }
+        }
+    }
+}
