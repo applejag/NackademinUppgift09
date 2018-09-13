f6a1690f928e56d42c31b202c8c0bb15b61c5c15
kalle (jag) <kalle.jillheden@me.com>
2018-09-13 18:15:36 +0200

    Added auktion&bud models

---
+++ Nauktion/Models/AuktionModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Nauktion.Models
+{
+    public class AuktionModel
+    {
+        public int AuktionID { get; set; }
+        public string Titel { get; set; }
+        public string Beskrivning { get; set; }
+        public DateTime StartDatum { get; set; }
+        public DateTime SlutDatum { get; set; }
+        public int Utropspris { get; set; }
+        public string SkapadAv { get; set; }
+        public int Gruppkod { get; set; }
+    }
+}
---
+++ Nauktion/Models/BudModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Nauktion.Models
+{
+    public class BudModel
+    {
+        public int BudID { get; set; }
+        public int AuktionID { get; set; }
+        public int Summa { get; set; }
+        public string Budgivare { get; set; }
+    }
+}
