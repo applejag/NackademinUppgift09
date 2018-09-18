d50a1552f5ef02e022a29973bc1dfa2184d325e6
kalle (jag) <kalle.jillheden@me.com>
2018-09-18 12:22:28 +0200

    Fixed incorrect model value type

--- Nauktion/Models/AuktionModel.cs
+++ Nauktion/Models/AuktionModel.cs
@@ -9,7 +9,7 @@ namespace Nauktion.Models
         public string Beskrivning { get; set; }
         public DateTime StartDatum { get; set; }
         public DateTime SlutDatum { get; set; }
-        public int Utropspris { get; set; }
+        public int? Utropspris { get; set; }
         public string SkapadAv { get; set; }
         public int Gruppkod { get; set; }
     }
