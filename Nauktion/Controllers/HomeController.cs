371d182619411942d47d4baf03237a3b7db4575b
kalle (jag) <kalle.jillheden@me.com>
2018-09-18 13:36:29 +0200

    Försvenskade massa texter

--- Nauktion/Controllers/HomeController.cs
+++ Nauktion/Controllers/HomeController.cs
@@ -17,15 +17,11 @@ namespace Nauktion.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
-
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
-
             return View();
         }
 
