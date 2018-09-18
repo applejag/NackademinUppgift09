4e86269e52e2d3f528cc9a263141b7f1bf281370
kalle (jag) <kalle.jillheden@me.com>
2018-09-18 13:49:38 +0200

    Fixed layout some

--- Nauktion/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ Nauktion/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -23,22 +23,11 @@ namespace Nauktion.Areas.Identity.Pages.Account
             _logger = logger;
         }
 
-        public void OnGet()
-        {
-        }
-
-        public async Task<IActionResult> OnPost(string returnUrl = null)
+        public async Task<IActionResult> OnGet(string returnUrl = null)
         {
             await _signInManager.SignOutAsync();
             _logger.LogInformation("User logged out.");
-            if (returnUrl != null)
-            {
-                return LocalRedirect(returnUrl);
-            }
-            else
-            {
-                return Page();
-            }
+            return Page();
         }
     }
 }
