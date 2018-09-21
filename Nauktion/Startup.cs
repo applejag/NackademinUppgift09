21c9fcb018757c10e0838fb9e0080fee7dc6e60c
kalle (jag) <kalle.jillheden@gmail.com>
2018-09-21 09:54:08 +0200

    Fixed localization, forreal this time

--- Nauktion/Startup.cs
+++ Nauktion/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -7,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -58,7 +60,26 @@ namespace Nauktion
                 app.UseHsts();
             }
 
-            app.UseRequestLocalization();
+            app.UseRequestLocalization(locOptions =>
+            {
+                var cultureInfoSvSe = new CultureInfo("sv-SE");
+                locOptions.DefaultRequestCulture = new RequestCulture(cultureInfoSvSe);
+                locOptions.SupportedCultures =
+                locOptions.SupportedUICultures = new[]
+                {
+                    new CultureInfo("en-US"),
+                    new CultureInfo("en-AU"),
+                    new CultureInfo("en-GB"),
+                    new CultureInfo("en"),
+                    new CultureInfo("es-ES"),
+                    new CultureInfo("es-MX"),
+                    new CultureInfo("es"),
+                    new CultureInfo("fr-FR"),
+                    new CultureInfo("fr"),
+                    cultureInfoSvSe,
+                    new CultureInfo("sv"),
+                };
+            });
             app.UseAuthentication();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
